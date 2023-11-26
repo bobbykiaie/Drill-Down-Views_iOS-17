3b79345dafdeaac8b9aec781d7153a05cec44b87
Bobby Kiaie <bobbykiaie@gmail.com>
2023-11-25 17:11:04 -0800

    Initial Commit

---
+++ Drill Down Views_iOS 17/Drill_Down_Views_iOS_17App.swift
@@ -0,0 +1,17 @@
+//
+//  Drill_Down_Views_iOS_17App.swift
+//  Drill Down Views_iOS 17
+//
+//  Created by Babak Kiaie on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Drill_Down_Views_iOS_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
